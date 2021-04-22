8a4402e7706e3246fe7a4820bc63893c0c41aeaa
melvinstark3 <iammelvin@pm.me>
2021-04-23 00:56:33 +0530

    Add Custom Widgets and loading icon
    Signed-off-by: melvinstark3 <iammelvin@pm.me>

---
+++ lib/customWidgets/customLoader.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:progress_indicators/progress_indicators.dart';
+
+class VirusLoader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GlowingProgressIndicator(
+        child: SizedBox(
+            height: 50.0,
+            child: Image(
+              height: MediaQuery.of(context).size.height * 0.02,
+              image: AssetImage('images/loader.png'),
+              color: Theme.of(context).accentColor,
+            )),
+      ),
+    );
+  }
+}
